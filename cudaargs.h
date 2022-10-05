589f7e8b9a390302ccc3e6de40a9e3f2a2ac2d8f
ForgotMyCode <79061723+ForgotMyCode@users.noreply.github.com>
2022-10-05 12:33:54 +0200

    Update cudaargs.h

--- cudaargs.h
+++ cudaargs.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef __INTELLISENSE__
-  #define CUDA_ARGS(...)
+#define CUDA_ARGS(...)
+#define CUDA_HIDE_ERRORS(CODE) {}
 #else
-  #define CUDA_ARGS(...) <<< __VA_ARGS__ >>>
+#define CUDA_ARGS(...) <<< __VA_ARGS__ >>>
+#define CUDA_HIDE_ERRORS(CODE) CODE
 #endif
